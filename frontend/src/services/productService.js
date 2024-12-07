13bc8bb47765fcb79ac9af90dd97f03e34759f19
Omar <Omar.Daghestani@colorado.edu>
2024-12-06 20:07:00 -0700

    finished the backend issue

--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/products'; 
+const API_URL = import.meta.env.VITE_API_URL; 
 
 // Fetch all products with optional query parameters
 export const fetchProducts = async (query = '') => {
