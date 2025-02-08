289e90049e3c6bf30d65eeb6fa92135f47f38d6b
OmarD <omar.daghestani@colorado.edu>
2025-02-08 08:29:04 -0700

    added local testing

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,7 +3,7 @@ const config = {
     port: process.env.PORT || 3001,
     jwtSecret: process.env.JWT_SECRET || 'defaultsecret',
     jwtExpiresIn: '30d',
-    dbUri: process.env.MONGO_URI,
+    dbUri: process.env.MONGO_URI || 'mongodb://localhost:27017/ecommerce'
   };
   
   export default config;
